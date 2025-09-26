cab707cae4483270f75565c7f034aa12c8017ec3
winun1127 <97441012+winun1127@users.noreply.github.com>
2025-09-26 10:36:08 +0900

    Test components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import {
+  Header,
+  HeaderLogo,
+  HeaderButton,
+  HeaderActions,
+  UserProfile,
+} from "@/components/header";
+import { LeftNavigationBar } from "@/components/left-navigation-bar";
+import {
+  MainContentArea,
+  MainContentBody,
+  MainContentHeader,
+} from "@/components/main-content-area";
+import TitleNavigator from "@/components/title-navigator";
+import { Card } from "@/components/card";
+
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <SidebarProvider>
+      <Header>
+        <HeaderLogo>E-Forest Logo</HeaderLogo>
+        <HeaderButton isOn={true}>공장종합</HeaderButton>
+        <HeaderButton>프레스</HeaderButton>
+        <HeaderButton>차체</HeaderButton>
+        <HeaderActions>
+          <UserProfile name="홍길동" part="E-Forest 전략팀" />
+        </HeaderActions>
+      </Header>
+      <LeftNavigationBar />
+      <MainContentArea>
+        <MainContentHeader>
+          <TitleNavigator />
+        </MainContentHeader>
+        <MainContentBody>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </MainContentBody>
+      </MainContentArea>
+    </SidebarProvider>
+  );
 }
 
 export default App;
